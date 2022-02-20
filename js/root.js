7f715951278225b79cb5349d84e9a512e6061f1d
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-20 16:05:14 +0530

    feat: added quiz category page

---
+++ js/root.js
@@ -0,0 +1,46 @@
+const themeIcon = document.querySelector(".theme-icon");
+const novaLogo = document.querySelectorAll(".logo");
+
+console.log(themeIcon);
+const getCurrentTheme = () => {
+  let theme = window.matchMedia("(prefers-color-scheme): light").matches
+    ? "light"
+    : "dark";
+  localStorage.getItem("nova.theme")
+    ? (theme = localStorage.getItem("nova.theme"))
+    : null;
+  console.log(theme);
+  return theme;
+};
+
+const loadTheme = (theme) => {
+  const root = document.querySelector(":root");
+  if (theme === "light") {
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+    novaLogo.forEach((item) => {
+      item.src = "../../assets/nova-logo-light.png";
+    });
+  } else {
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+    novaLogo.forEach((item) => {
+      item.src = "../../assets/nova-logo-dark.png";
+    });
+  }
+  root.setAttribute("color-scheme", `${theme}`);
+};
+
+themeIcon.addEventListener("click", () => {
+  let theme = getCurrentTheme();
+  if (theme === "dark") {
+    theme = "light";
+  } else {
+    theme = "dark";
+  }
+  localStorage.setItem("nova.theme", `${theme}`);
+  loadTheme(theme);
+});
+window.addEventListener("DOMContentLoaded", () => {
+  loadTheme(getCurrentTheme());
+});
